8ab5d69f46d03b0d64392d64f207b818b8b454f5
Nanami Tomozoe <nanami@tomozoe.com>
2017-05-05 04:14:34 +0900

    Initial Commit

---
+++ Lifelog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lifelog
+//
+//  Created by nanami on 2017/05/05.
+//  Copyright © 2017年 nanami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
