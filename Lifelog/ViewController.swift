8e11c2e45057ad8ac65e3299289e9fd4c25ae401
Nanami Tomozoe <nanami@tomozoe.com>
2017-05-05 05:20:51 +0900

    Tableviewエラってる・・・。

--- Lifelog/ViewController.swift
+++ Lifelog/ViewController.swift
@@ -8,17 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
 
+    @IBOutlet var table:UITableView!
+    
+    var imgArray:NSArray = ["dog1.jpg","cat1.jpg"]
+    var imageColler:NSArray = ["赤色.jpg"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    //Table Viewのセルの数を指定
+    func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imgArray.count
+    }
+    
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "tablecell", for: indexPath)
+        
+        let img = UIImage(named:"\(imgArray[indexPath.row])")
+        //let img2 = UIImage(named:"\(imageColler[indexPath.row])")
+        
+        // Tag番号 1 で UIImageView インスタンスの生成
+        let imageView = table.viewWithTag(1) as! UIImageView
+        imageView.image = img
+        
+        // Tag番号 ２ で UILabel インスタンスの生成
+      //  let imageView2 = table.viewWithTag(2) as! UIImageView
+      //  imageView2.image = img2
+       
+        /* // Tag番号 ３ で UILabel インスタンスの生成
+        let label2 = table.viewWithTag(3) as! UILabel
+        label2.text = "\(imageTitles[indexPath.row])"*/
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
 
 
 }
