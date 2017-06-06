d6b29484f4641454652dd14039e172413ef4bdc7
Nanami Tomozoe <nanami@tomozoe.com>
2017-06-06 23:44:35 +0900

    メモ場所移動

--- Lifelog/ViewController.swift
+++ Lifelog/ViewController.swift
@@ -50,40 +50,7 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         }
     
     
-   //UserDefaults保存関係↓
-   /* public func UIImagePNGRepresentation(image: UIImage) -> Data?{
-       
-     }*/
-    
-    /*func saveImage3(pn:Int,imgs:[UIImage]){
-        // UserDefaultsを使用できるようにするe
-        let defaults = UserDefaults.standard
-        // [UIImage] → [NSData]
-        
-        // let dataImages: [NSData] = photos.map { (image) -> NSData in
-        let dataImages: [Data] = imgArray.map{ (image) -> Data in
-            UIKit.UIImagePNGRepresentation(image)!
-        }
-        
-        //
-        let photosName:String = "imgArray" + String(pn)//保存名を決定
-        defaults.set(dataImages, forKey: photosName)
-        
-    }*/
-   /* func saveImage3(pn:Int,imgs:[UIImage]){
-        let photos = imgs
-        let photoData: UserDefaults = UserDefaults.standard
-        // [UIImage] → [NSData]
-        
-        // let dataImages: [NSData] = photos.map { (image) -> NSData in
-        let dataImages: [Data] = photos.map { (image) -> Data in
-            UIKit.UIImagePNGRepresentation(image)!
-        }
-        //
-        let photosName:String = "photos" + String(pn)//保存名を決定
-        photoData.set(dataImages, forKey: photosName)
-        
-        }*/
+
     
     
     
@@ -104,18 +71,7 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         imageColler.insert("赤色.jpg", at: 0)
         print(imgArray)
         
-        
-       /* let userDefaults = UserDefaults.standard
-        let imageData = UIImagePNGRepresentation(capturedImage)
-        userDefaults.set(imageData, forKey: "imageData")
-        userDefaults.synchronize()
-        
-        print(userDefaults,"Hey!!!")
-        
-        if (userDefaults.object(forKey: "imageData") != nil) {
-            print("データ有り")
-        }*/
-        
+ 
         let userDefaults = UserDefaults.standard
 
         // [UIImage] → [Data]
@@ -128,10 +84,7 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         userDefaults.synchronize()
         print(userDefaults,"←写真保存")
         
-        //imgArray =  userDefaults.objectForKey("imageData") as! [NSData]
-   
-        // UserDefaultsから値を読み込む
-        //imgArray = userDefaults.object(forKey: "imageData") as! [UIImage]
+     
           test = userDefaults.object(forKey: "imageData") as! [Data]
         print(test,"←保存できてる？")
 
@@ -203,6 +156,44 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
     }
     
     
+    
+    
+    
+   //いらんやつ
+    //UserDefaults保存関係↓
+    /* public func UIImagePNGRepresentation(image: UIImage) -> Data?{
+     
+     }*/
+    
+    /*func saveImage3(pn:Int,imgs:[UIImage]){
+     // UserDefaultsを使用できるようにするe
+     let defaults = UserDefaults.standard
+     // [UIImage] → [NSData]
+     
+     // let dataImages: [NSData] = photos.map { (image) -> NSData in
+     let dataImages: [Data] = imgArray.map{ (image) -> Data in
+     UIKit.UIImagePNGRepresentation(image)!
+     }
+     
+     //
+     let photosName:String = "imgArray" + String(pn)//保存名を決定
+     defaults.set(dataImages, forKey: photosName)
+     
+     }*/
+    /* func saveImage3(pn:Int,imgs:[UIImage]){
+     let photos = imgs
+     let photoData: UserDefaults = UserDefaults.standard
+     // [UIImage] → [NSData]
+     
+     // let dataImages: [NSData] = photos.map { (image) -> NSData in
+     let dataImages: [Data] = photos.map { (image) -> Data in
+     UIKit.UIImagePNGRepresentation(image)!
+     }
+     //
+     let photosName:String = "photos" + String(pn)//保存名を決定
+     photoData.set(dataImages, forKey: photosName)
+     
+     }*/
   
 
 
