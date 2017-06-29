1a8e7457c766b8bbb983cd0295a7a0647419a57c
Nanami Tomozoe <nanami@tomozoe.com>
2017-06-29 19:28:04 +0900

    理解しやすくコメント追加　

--- Lifelog/ViewController.swift
+++ Lifelog/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
     
     
     //配列上２個やのに下１個しか入ってない時、Tableviewセルの数２こやと動かない
-    //var imgArray:[UIImage] = [dog1.jpg,cat1.jpg]
-    var imgArray:[UIImage] = []
+    //var imageArray:[UIImage] = [dog1.jpg,cat1.jpg]
+    var imageArray:[UIImage] = []
     var imageColler:Array = ["赤色.jpg","赤色.jpg","赤色.jpg","赤色.jpg","赤色.jpg"]
     var test:[Data] = []
     
@@ -49,46 +49,60 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
     
   override func viewWillAppear(_ animated: Bool) {
    
-    // !=はノットイコールやから   nilではない = 画像がある という意味
+    // !=はノットイコールやから   [nilではない = 画像がある] という意味
     //capturedImageがあったら表示
     if  capturedImage != nil {
+       
+        //配列の指定の箇所に要素を挿入(insert)する
+        imageArray.insert(capturedImage, at: 0)
+      //imageColler.insert("赤色.jpg", at: 0)
+        print(imageArray)
         
-        imgArray.insert(capturedImage, at: 0)
-      //  imageColler.insert("赤色.jpg", at: 0)
-        print(imgArray)
-        
- 
+        //インスタンスの作成→UserDefaultsの宣言
         let userDefaults = UserDefaults.standard
         
+        //!=はノットイコールやから   [nilではない = 画像がある] という意味
         //今までに保存したデータがあるか確認　もしあったら
-        //!=はノットイコールやから   nilではない = 画像がある という意味
         if (userDefaults.object(forKey: "imageData") != nil){
             print("データあり")
             //savedDataにuserDefaultsを呼び出す
             let savedData = userDefaults.object(forKey: "imageData")as![Data]
-            //imgArrayにuserDefaultsに保存されていたデータを追加（[savedData]保存されている数だけ追加）
+            
+            //imageArrayにuserDefaultsに保存されていたデータを追加（[savedData]保存されている数だけ追加）
             for i in 0..<savedData.count {
                 
-                imgArray.append(UIImage(data:savedData[i])!)
+                //入ってる配列の後ろに要素を追加append
+                imageArray.append(UIImage(data:savedData[i])!)
                 
             }
+         /*-----------------for-in ~指定範囲から順に値を取り出して実行------------------
+             for 変数 in 開始値..<最終値 { // 最終値を含まない  （...←この点が三つやと最終値含む）
+             // 処理
+             }
+            
+         ------------------------------------*/
         }
         //二回目の場合　新しいのに古いのを追加した完成版を改めて保存
+        
         // [UIImage] → [Data]
-        let data: [Data] = imgArray.map { (image) -> Data in
+        let data: [Data] = imageArray.map { (image) -> Data in
             UIImagePNGRepresentation(image)!
         }
         
+        //dataを[imageData]というキーでuserDefaultに保存
         userDefaults.set(data, forKey: "imageData")
+        
         //userDefaultsをplistファイルへの出力と同期する。
         userDefaults.synchronize()
         print(userDefaults,"←写真保存")
         
-        //imgArray =  userDefaults.objectForKey("imageData") as! [NSData]
+        //imageArray =  userDefaults.objectForKey("imageData") as! [NSData]
    
         // UserDefaultsから値を読み込む
-        //imgArray = userDefaults.object(forKey: "imageData") as! [UIImage]
-          test = userDefaults.object(forKey: "imageData") as! [Data]
+        //imageArray = userDefaults.object(forKey: "imageData") as! [UIImage]
+        
+        //↓これいらんかも
+         test = userDefaults.object(forKey: "imageData") as! [Data]
         print(test,"←保存できてる？")
 
         
@@ -101,9 +115,9 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
     }
     
     
-    //Table Viewのセルの数を指定 imgArrayの数だけtableViewのセルを作る
+    //Table Viewのセルの数を指定 imageArrayの数だけtableViewのセルを作る
     func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imgArray.count
+        return imageArray.count
         
         
     }
@@ -125,11 +139,11 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         
         // Tag番号 1 で UIImageView インスタンスの生成
         let imageView = table.viewWithTag(1) as! UIImageView
-       // imgArrayの配列の画像をcellの行ごとに（indexPath.row）、imageViewに追加して行く
-        //imageView.image = imgArray[indexPath.row]
+       // imageArrayの配列の画像をcellの行ごとに（indexPath.row）、imageViewに追加して行く
+        //imageView.image = imageArray[indexPath.row]
        // print(indexPath.row,"←indexPath.row　行数入ってた")
        // imageView.image =  UIImage(data: test[indexPath.row])
-         imageView.image =  imgArray[indexPath.row]
+         imageView.image =  imageArray[indexPath.row]
         
         // Tag番号 ２ で UILabel インスタンスの生成
         let imageView2 = table.viewWithTag(2) as! UIImageView
@@ -163,7 +177,7 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
             // 削除
-             imgArray.remove(at: indexPath.row)
+             imageArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             
             // データの保存
